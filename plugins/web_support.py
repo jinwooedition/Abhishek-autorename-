b21385e4d6d90c4536b50ea167286255dbf5fdfc
jinwooedition <abhi1508ak@gmail.com>
2025-03-04 13:41:33 +0530

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -1,2 +1,9 @@
-def web_server():
-    print("Web server is running")
+from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="Hello, world")
+
+async def web_server():
+    app = web.Application()
+    app.router.add_get('/', handle)
+    return app
